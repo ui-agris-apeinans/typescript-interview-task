bba6c64d0ee124554a5e4ba8f8792189e65491ac
Agris Apeinans <agrisapeinans@Agriss-MacBook-Air.local>
2022-02-01 17:54:12 +0200

    improve change pass ux - add error, etc.

--- src/components/PasswordHealth/components/List/List.tsx
+++ src/components/PasswordHealth/components/List/List.tsx
@@ -1,18 +1,19 @@
-import { FC, useState } from 'react';
+import { FC, SyntheticEvent, useState } from 'react';
 import Modal from 'react-modal';
 
 import { IItem } from "~/services/userItems";
 import ItemIcon from './components/ItemIcon';
+import ErrorBlock from "~/components/ErrorBlock";
 import './list-style.scss';
 
 interface IList {
   items: Array<IItem>;
-  updateItems: (IItem) => void;
+  updateItems: (item: IItem) => void;
 }
 
 interface IUpdateModal {
   item: IItem;
-  updateItems: (IItem) => void;
+  updateItems: (item: IItem) => void;
 }
 
 Modal.setAppElement('body');
@@ -20,6 +21,28 @@ Modal.setAppElement('body');
 const UpdateModal: FC<IUpdateModal> = ({ item, updateItems }) => {
   const [showModal, setShowModal] = useState(false);
   const [newPass, setNewPass] = useState('');
+  const [error, setError] = useState('');
+
+  const onSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!newPass.length) {
+      setError('Password must be at least 1 character')
+    } else {
+      updateItems({
+        ...item,
+        password: newPass,
+      })
+      setShowModal(false)
+    }
+  }
+
+  const onCloseModal = () => {
+    console.log('close')
+    setNewPass('');
+    setError('');
+    setShowModal(false)
+  }
 
   return (
     <>
@@ -29,32 +52,29 @@ const UpdateModal: FC<IUpdateModal> = ({ item, updateItems }) => {
       <Modal
         className="modal"
         isOpen={showModal}
-        onRequestClose={() => setShowModal(false)}
+        onRequestClose={onCloseModal}
         contentLabel="Example Modal"
       >
-        <h1>Update Password</h1>
-        <input
-          placeholder="new password"
-          className="input"
-          type="password"
-          value={newPass}
-          onChange={(event) => setNewPass(event.target.value)}
-        />
-        <div className="pt-12px text-center">
-          <button className="button" onClick={() => {
-            updateItems({
-              ...item,
-              password: newPass,
-            })
-            setShowModal(false)
-          }}>Change</button>
-          <button className="button ml-12px" onClick={() => {
-            setNewPass('');
-            setShowModal(false)
-          }}>
-            Cancel
-          </button>
-        </div>
+        <form onSubmit={onSubmit}>
+          <h1>Update Password</h1>
+          <input
+            placeholder="new password"
+            className="input"
+            type="password"
+            minLength={1}
+            value={newPass}
+            onChange={(event) => setNewPass(event.target.value)}
+          />
+
+          <ErrorBlock error={error} />
+
+          <div className="pt-12px text-center">
+            <button className="button" type="submit">Change</button>
+            <button className="button ml-12px" onClick={onCloseModal}>
+              Cancel
+            </button>
+          </div>
+        </form>
       </Modal>
     </>
   );
