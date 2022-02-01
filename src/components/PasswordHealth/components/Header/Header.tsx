855743b7f323da43bcec66d18b273257106c8eda
Agris Apeinans <agrisapeinans@Agriss-MacBook-Air.local>
2022-02-01 17:14:23 +0200

    fix total weak pass count

--- src/components/PasswordHealth/components/Header/Header.tsx
+++ src/components/PasswordHealth/components/Header/Header.tsx
@@ -2,6 +2,9 @@ import { FC } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { IItem } from "~/services/userItems";
+import itemHasWeakPassword from "~/utils/itemHasWeakPassword";
+import itemHasReusedPassword from "~/utils/itemHasReusedPassword";
+import itemHasOldPassword from "~/utils/itemHasOldPassword";
 import logout from '../../../../services/logout';
 
 import './header-style.scss';
@@ -13,13 +16,14 @@ interface IHeader {
 
 const Header: FC<IHeader> = ({ items, username }) => {
   const { push } = useHistory()
+  const vulnerableItemCount = items.filter(item => itemHasOldPassword(item) || itemHasReusedPassword(item, items) || itemHasWeakPassword(item)).length
 
   return (
     <div className="header">
       <div className="user-section">
         <button onClick={() => logout(push)}>{`Logout ${username}`}</button>
       </div>
-      <h1>{`${items.length} Items are vulnerable`}</h1>
+      <h1>{`${vulnerableItemCount} Items are vulnerable`}</h1>
       <span>Create new complex passwords to protect your accounts</span>
     </div>
   )
