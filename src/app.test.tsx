971b90ff4a114049603e84981cd778644cd729e7
Agris Apeinans <agrisapeinans@Agriss-MacBook-Air.local>
2022-02-02 16:03:09 +0200

    add testing library, app test

---
+++ src/app.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders App', () => {
+    render(<App />);
+    const appElement = screen.getByTestId('app');
+
+    expect(appElement).toBeInTheDocument();
+});
